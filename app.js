eaaca8426383c25fee56a245f79d13a97d6d4466
jiangyuezhu <jiangyue.zhu@flite.com>
2013-09-18 12:49:33 -0700

    forward

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+var util = require('util'),
+    colors = require('colors'),
+    http = require('http'),
+    httpProxy = require('http-proxy'),
+    url = require('url');
 
 //
-// Create a proxy server with custom application logic
+// Setup proxy server with forwarding
 //
-//httpProxy.createServer(function (req, res, proxy) {
-  //
-  // Put your custom server logic here
-  //
-
-var options = {
-  router: {
-    'localhost': '127.0.0.1:9000'
+httpProxy.createServer(9000, 'localhost', {
+  forward: {
+    port: 8080,
+    host: 'localhost/t'
   }
-};
+}).listen(8082);
 
-var proxyServer = httpProxy.createServer(options);
-proxyServer.listen(8081);
-
-http.createServer(function (req, res) { 
+//
+// Target Http Server
+//
+http.createServer(function (req, res) {  
+  console.log(url.parse(req.url, true)); 
+  console.log('\n');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
   res.end();
 }).listen(9000);
+
+
+/*util.puts('http proxy server '.blue + 'started '.green.bold + 'on port '.blue + '8003 '.yellow + 'with forward proxy'.magenta.underline);
+util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '9000 '.yellow);
+util.puts('http forward server '.blue + 'started '.green.bold + 'on port '.blue + '9001 '.yellow);*/
